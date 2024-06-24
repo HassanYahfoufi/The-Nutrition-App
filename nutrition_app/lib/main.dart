fba9b2405c5369c6a0b993b2d91e40693927a5fd
dug0004 <DanielGarcia20@my.unt.edu>
2024-06-24 11:57:06 -0500

    Made view food items page(empty shell)

--- nutrition_app/lib/main.dart
+++ nutrition_app/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:nutrition_app/register_page.dart';
 import 'package:nutrition_app/settings_page.dart';
 import 'package:nutrition_app/login_page.dart';
 import 'package:nutrition_app/add_food_item.dart';
+import 'package:nutrition_app/view_food_items.dart';
 import 'package:nutrition_app/database_helper.dart';
 import 'package:nutrition_app/models.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
--- nutrition_app/lib/view_food_items.dart
+++ nutrition_app/lib/view_food_items.dart
@@ -1,1 +1,20 @@
+import 'package:flutter/material.dart';
 
+class ViewFoodItems extends StatefulWidget {
+  const ViewFoodItems({super.key});
+
+  @override
+  State<ViewFoodItems> createState() => _ViewFoodItemsState();
+}
+
+class _ViewFoodItemsState extends State<ViewFoodItems> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("View Food Items"),
+        backgroundColor: Colors.green,
+      )
+    );
+  }
+}
